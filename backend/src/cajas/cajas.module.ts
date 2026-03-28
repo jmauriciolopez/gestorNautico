da23ca2468f1748d53366c7f9d532d055542173d
MauricioLopez <jmauricio_lopez@hotmail.com>
2026-03-28 20:38:42 -0300

    cmpi

--- backend/src/cajas/cajas.module.ts
+++ backend/src/cajas/cajas.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Caja } from './caja.entity';
 import { CajasService } from './cajas.service';
 import { CajasController } from './cajas.controller';
+import { NotificacionesModule } from '../notificaciones/notificaciones.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Caja])],
+  imports: [TypeOrmModule.forFeature([Caja]), NotificacionesModule],
   controllers: [CajasController],
   providers: [CajasService],
   exports: [CajasService],
